e0e5997b07304c0757adf4fd3a429951b5a8b9a7
qgarner <garnerq94@icloud.com>
2023-05-21 10:44:55 -0500

    Test Run

---
+++ Bash/google.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Choose a number between 2-4"
+read $number
+$2 xdg-open http://google.com
+$3 xdg-open https://skillsforall.com/dashboard
+$4 xdg-open https://canvas.instructure.com/
+
+
