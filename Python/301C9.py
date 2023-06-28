e2194fc7b5cdfd6c7b2fcd0b29e01fdaabc69a62
Quin Garner <garnerq94@icloud.com>
2023-06-27 20:48:38 -0500

    Changing and printing list

---
+++ Python/301C9.py
@@ -0,0 +1,24 @@
+#Quinton Garner
+#Class 301 Lab 9
+
+# Create a Python script that includes the following:
+# Assign to a variable a list of ten string elements.
+# Print the fourth element of the list.
+# Print the sixth through tenth element of the list.
+# Change the value of the seventh element to “onion”.
+
+# Assign to a variable a list of ten string elements.
+myList=['Kobe','Lebron','Jordan','Durant','Luca','Himmy Butler','Giannis','Jokic','Wemby','Morant']
+
+# Print the fourth element of the list.
+fourthElement=myList[3]
+print(fourthElement)
+
+# Print the sixth through tenth element of the list.
+sixElement = myList[5]
+print(sixElement)
+
+# Change the value of the seventh element to “onion”.
+myList[6] = 'onion'
+print(myList)
+
